4c9993bf7915ed0e0ac1e8955399cd5a40135b0b
your_username <annwrst@gmail.com>
2021-09-26 02:05:44 +0300

    add placeholders to db requests

--- src/db-requests.js
+++ src/db-requests.js
@@ -3,7 +3,7 @@ import writeLog from "./logger.js";
 
 export async function addToDB(url, description, domain, price, oldPrice, chatID) {
     try {
-        if (sqlite.run(`SELECT url FROM sales WHERE user_id=${chatID} AND url='${url}'`).length !== 0) return false;
+        if (sqlite.run(`SELECT url FROM sales WHERE user_id= ? AND url= ?`, [chatID, url]).length !== 0) return false;
         else {
             const db_request = `INSERT INTO sales(url, description, domain, new_price, old_price, user_id) VALUES('${url}', '${description}', '${domain}', ${price}, ${oldPrice},${chatID});`;
             console.log(sqlite.run(db_request));
@@ -17,7 +17,7 @@ export async function addToDB(url, description, domain, price, oldPrice, chatID)
 
 export async function getList(userId) {
     try {
-        return sqlite.run(`SELECT * FROM sales WHERE user_id="${userId}"`);
+        return sqlite.run(`SELECT * FROM sales WHERE user_id= ? `, [userId]);
     } catch (e) {
         throw Error("There is an error getting the list");
     }
@@ -25,7 +25,7 @@ export async function getList(userId) {
 
 export function deleteItem(id) {
     try {
-        sqlite.run(`DELETE FROM sales WHERE id=${id}`);
+        sqlite.run(`DELETE FROM sales WHERE id= ? `, [id]);
     } catch {
         return false;
     }
@@ -34,7 +34,7 @@ export function deleteItem(id) {
 
 export function updateItem(id, new_price, old_price) {
     try {
-        sqlite.run(`UPDATE sales SET new_price = ${+new_price}, old_price = ${+old_price} WHERE id=${id};`);
+        sqlite.run(`UPDATE sales SET new_price = ? , old_price = ? WHERE id= ? ;`, [+new_price, +old_price, +id]);
     } catch {
         return false;
     }
